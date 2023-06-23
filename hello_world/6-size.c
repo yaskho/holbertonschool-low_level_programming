4563266a761b1d176eec976be777204c38fc4282
yaskho <yassineholberton@gmail.com>
2023-06-23 01:47:27 -0700

    y size 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,11 +1,17 @@
-!/bin/bash
+!#/bin/bash
 #include <stdio.h>
-int main(void) 
+/**
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
 	printf("Size of char: %zu byte(s)\n", sizeof(char));
 	printf("Size of int: %zu byte(s)\n", sizeof(int));
 	printf("Size of long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
