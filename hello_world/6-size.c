853a37e447fd3d76120f1749e9cab47da744090b
yaskho <yassineholberton@gmail.com>
2023-06-23 01:54:39 -0700

    my size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,4 +1,4 @@
-!#/bin/bash
+
 #include <stdio.h>
 /**
  * main - Prints the size of various types based on
@@ -6,6 +6,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
