f349acde006831adf9cc4dda69b416836246183f
yaskho <yassinehoberton@gmail.com>
2023-07-05 03:09:01 -0700

    my task6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-int main(void) 
+/**
+*prints all single digit numbers of base 10 starting from 0,
+*followed by a new line.
+*/
 
+int main(void)
 {
-
 	int n;
 
 	for (n = 0; n < 10; n++)
-{
-	putchar("n");
-}
+	{
+	putchar(n);
+	}
 
 	putchar('\n');
-	return 0;
+
+	return (0);
 }
 
