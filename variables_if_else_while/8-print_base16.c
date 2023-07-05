ebfaafa6612c60b6d715667c79babd7cda23c19b
yaskho <yassinehoberton@gmail.com>
2023-07-05 06:56:43 -0700

    my task 9

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -3,24 +3,24 @@
 /**
 *main- prints all the numbers of base 16 in lowercase.
 *followed by a new line using putchar
-*Return:0
+*Return: 0
 */
 int main(void)
 
 {
 	char digit;
 
-	for (digit = '0'; digit <= '9'; digit++) 
+	for (digit = '0'; digit <= '9'; digit++)
 {
 	putchar(digit);
 }
 
-	for (digit = 'a'; digit <= 'f'; digit++) 
+	for (digit = 'a'; digit <= 'f'; digit++)
 {
 	putchar(digit);
 }
 
 	putchar('\n');
 
-	return 0;
+	return (0);
 }
