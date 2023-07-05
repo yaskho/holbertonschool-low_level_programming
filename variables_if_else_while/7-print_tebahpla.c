2659967f86b4030710cb94cac5722538f8d24a12
yaskho <yassinehoberton@gmail.com>
2023-07-05 04:17:28 -0700

    my task7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*prints the lowercase alphabet in reverse
+*followed by a new line by putchar
+*return: 0
+*/
+
+int main(void)
+{
+	char letter;
+	for (lettre = 'z'; letter <= 'a'; letter--) 
+	{
+
+	putchar(letter);
+
+	}
+	putchar('/n');
+	return (0);
+}
