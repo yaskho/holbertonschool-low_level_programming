6f616f66e8f321bde16ed960fd18246e21f81ab9
yaskho <yassinehoberton@gmail.com>
2023-07-08 12:27:37 -0700

    my task2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*main - check the code.
+*print_alphabet_x10
+*Return: Always 0.
+*/
+void print_alphabet_x10(void)
+
+{
+
+	for (int i = 0; i < 10; i++)
+
+	for (char c = 'a'; c <= 'z'; c++)
+
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+}
