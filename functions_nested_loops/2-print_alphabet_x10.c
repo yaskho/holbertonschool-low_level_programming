bf4388c62d98eb1353e4655c36161c1f14252dc2
yaskho <yassinehoberton@gmail.com>
2023-07-08 13:32:46 -0700

    my task2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
-*main = check the code.
-*print_alphabet_x10 - function
+*print_alphabet_x10 - function that prints 10 times the alphabet, in lowercase, followed by a new line.
 *Return: Always 0.
 */
 
@@ -12,7 +11,7 @@ void print_alphabet_x10(void)
 	char c;
 
 	for (i = 0; i < 10; i++)
-	
+
 	{
 
 		for (c = 'a'; c <= 'z'; c++)
