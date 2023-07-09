80f619cd0add9da154d67658159197b5d7bb408e
yaskho <yassinehoberton@gmail.com>
2023-07-09 16:18:38 -0700

    my task7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,13 @@ int print_last_digit(int n)
 	int last_digit;
 
 	if (n < 0)
+
+	{
 		n = -n;
+	}
 
-		last_digit = n % 10;
-		_putchar(last_digit + '0');
+	last_digit = n % 10;
+	_putchar(last_digit + '0');
 
-	return last_digit;
+	return (last_digit);
 }
