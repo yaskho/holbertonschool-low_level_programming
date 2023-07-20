2be7d2e4ca86d12ebbe1e8c9a4bc525eedfd34e3
yaskho <yassineholberton@gmail.com>
2023-07-20 21:26:25 +0100

    my task5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - function that reverses a string.
+ * @s: checking string
+*/
+
+void rev_string(char *s)
+{
+    int length = 0;
+    char temp;
+
+    while (s[length] != '\0')
+    {
+        length++;
+    }
+
+    for (int i = 0; i < length / 2; i++)
+    {
+        temp = s[i];
+        s[i] = s[length - 1 - i];
+        s[length - 1 - i] = temp;
+    }
+}
