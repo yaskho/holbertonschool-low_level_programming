ead9186fe17849acda4ebfd380a519ecc734e943
yaskho <yassineholberton@gmail.com>
2023-07-20 12:48:39 +0100

    my task0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,12 +3,12 @@
 /**
  * reset_to_98 - reset to 98
  *
- * @n: the integer to rest
- * 
+ * @p: the integer to rest
+ *
  * Return: 98
 */
 
 void reset_to_98(int *p)
 {
-    (*p) = 98;
+	(*p) = 98;
 }
