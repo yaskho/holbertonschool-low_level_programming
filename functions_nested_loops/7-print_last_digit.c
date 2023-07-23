9aca719439b03ac0c93fd4a91f624105f1650eb8
yaskho <yassineholberton@gmail.com>
2023-07-23 18:40:35 +0100

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,18 +7,19 @@
 */
 
 int print_last_digit(int n)
-
 {
 	int last_digit;
 
 	if (n < 0)
 
 	{
+		n %= 10;
 		n = -n;
+		last_digit = n;
 	}
-
+	else
+	{
 	last_digit = n % 10;
-	_putchar(last_digit + '0');
-
+	}
 	return (last_digit);
 }
