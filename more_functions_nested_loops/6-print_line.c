bc095ce4cddc87c82a66bcffb40ed6a4e367d5c0
yaskho <yassineholberton@gmail.com>
2023-07-23 20:36:09 +0100

    line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,18 +7,16 @@
 void print_line(int n)
 {
 	int i;
+
 	for (i = 0; i < n; i++)
 	{
 		_putchar('_');
 	}
-
-		_putchar('\n');
-
 	if (n <= 0)
 		{
 			_putchar('\n');
 
 			return;
 		}
-
+	_putchar('\n');
 }
