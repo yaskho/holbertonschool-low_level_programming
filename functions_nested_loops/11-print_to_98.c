093ab2e7bde3dd2f177f0bbdb4e1ba1ecb33bbc4
yaskho <yassineholberton@gmail.com>
2023-07-23 20:07:41 +0100

    my tasks

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,9 +2,9 @@
 #include<stdio.h>
 
 /**
-*printf - function that prints all natural numbers from n to 98
+*print_to_98 - function that prints all natural numbers from n to 98
 *@n:integer to check
-*Return:always 0
+*Return: always 0
 */
 void print_to_98(int n)
 {
