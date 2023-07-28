449d257fbf362e63bffaf328b0b1f7d13307115c
yaskho <yassineholberton@gmail.com>
2023-07-28 10:38:52 +0100

    my task1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: the string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
