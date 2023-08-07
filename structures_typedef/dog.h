9fcd44c0a7173abbc8b84ec7e640de894b4fd9fa
yaskho <yassineholberton@gmail.com>
2023-08-07 11:14:46 +0100

    dog header file

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/**
+* struct dog - defines a struct holding
+* dog description
+* @name: dogs name
+* @age: dogs age
+* @owner: dogs owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
