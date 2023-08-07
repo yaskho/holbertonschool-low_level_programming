0c6d16eadd575cc65d30cc691b27e6d64bcfb1d8
Your Name <yassineholberton@gmail.com>
2023-08-07 14:41:28 +0100

    creation

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,11 +15,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	    return (NULL);
     d->name = (char *) malloc(sizeof(char) * (strlen(name) + 1));
     if (d->name == NULL)
+    {
+        free(d);
         return (NULL);
+    }
     d->owner = (char *) malloc(sizeof(char) * (strlen(owner) + 1));
      if (d->owner == NULL)
      {
         free(d->name);
+        free(d);
         return (NULL);
      }
     strcpy(d->name, name);
