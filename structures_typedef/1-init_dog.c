805e9b2f775bf518c62467d5f1312e63e509c8b5
Your Name <yassineholberton@gmail.com>
2023-08-07 13:07:40 +0100

    my task1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+
+/**
+*init_dog - initialise a dog struct
+*@d: the struct
+*@name: dogs name
+*@age: dogs age
+*@owner: dogs owner
+*/
+
+#include "dog.h"
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
