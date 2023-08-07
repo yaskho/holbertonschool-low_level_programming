94831159a14cd3a12cc71c583d37b57bd6181b66
Your Name <yassineholberton@gmail.com>
2023-08-07 14:31:59 +0100

    create dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+/**
+*newdog - creates a new dog
+*@name: dogs name
+*@age: dogs age
+*@owner:dogs owner
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+    dog_t *d = NULL;
+
+    d->name = (char *) malloc(sizeof(char) * (strlen(name) + 1));
+    if (d->name == NULL)
+        return (NULL);
+    d->owner = (char *) malloc(sizeof(char) * (strlen(owner) + 1));
+     if (d->owner == NULL)
+        return (NULL);
+    strcpy(d->name, name);
+    strcpy(d->owner, owner);
+    d->age = age;
+    return (d);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef HEADER_H
 #define HEADER_H
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
 * struct dog - defines a struct holding
@@ -20,4 +22,5 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
