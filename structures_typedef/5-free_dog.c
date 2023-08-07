c10c87c6aabfef4732fb3d5c0dedbe617abc9ae0
Your Name <yassineholberton@gmail.com>
2023-08-07 14:50:08 +0100

    free dog

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -6,9 +6,9 @@
 #include "dog.h"
 void free_dog(dog_t *d)
 {
-    if (!d)
-        return;
-    free(d->owner);
-    free(d->name);
-    free(d);
+	if (!d)
+		return;
+	free(d->owner);
+	free(d->name);
+	free(d);
 }
