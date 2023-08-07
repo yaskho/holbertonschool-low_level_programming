738d5474364e2c3874d2fde9440a4b7ac21fffcd
Your Name <yassineholberton@gmail.com>
2023-08-07 14:35:34 +0100

    my task4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,14 +8,20 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *d = NULL;
+    dog_t *d;
 
+    d = malloc(sizeof(dog_t));
+	if (!d)
+	    return (NULL);
     d->name = (char *) malloc(sizeof(char) * (strlen(name) + 1));
     if (d->name == NULL)
         return (NULL);
     d->owner = (char *) malloc(sizeof(char) * (strlen(owner) + 1));
      if (d->owner == NULL)
+     {
+        free(d->name);
         return (NULL);
+     }
     strcpy(d->name, name);
     strcpy(d->owner, owner);
     d->age = age;
