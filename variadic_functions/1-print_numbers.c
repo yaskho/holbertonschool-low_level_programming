6f8b590bcfa775ba5009739161b21f12f862984c
Your Name <yassineholberton@gmail.com>
2023-08-12 23:35:26 +0100

    my task1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -4,23 +4,25 @@
 *print_numbers - function to print numbers
 *@separator:string
 *@n: variable
-*@a:variable
-*va_list - Declare a va_list
-*va_start - Initialize the va_list
-*va_end - clean the va_list
 */
 void print_numbers(const char *separator, const unsigned int n, ...)
+
 {
 	unsigned int i;
 	int a;
 	va_list args;
-{
-	va_start(args, n);
-	for (i = 0; i < n; i++)
-	a = va_arg(arg, int);
-	if (separator != NULL && i != n - 1)
-	printf("%d%s", a, separator);
-}
 
-		putchar('\n');
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		a = va_arg(args, int);
+		if (separator != NULL && i != n - 1)
+			printf("%d%s", a, separator);
+		else
+			printf("%d", a);
+	}
+	va_end(args);
+
+	putchar('\n');
 }
