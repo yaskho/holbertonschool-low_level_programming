80f27b681d2e21584381cc82c02f271e516a6369
Your Name <yassineholberton@gmail.com>
2023-08-12 23:52:39 +0100

    my task2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,34 +1,34 @@
 #include<stdio.h>
 #include"variadic_functions.h"
 #include<stdarg.h>
-#include<string.h>
 /**
 *print_strings - function to print strings
 *@separator: string
 *@n:variable
-*va_list - Declare a va_list
-*va_start - Initialize the va_list
-*va_end - clean the va_list
 */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-va_list args;
-char s;
-unsigned int i;
-va_start(args, n);
-for (i = 0;i < n; i++)
-{
-s = va_arg(args ,char *);
-if (s != NULL)
-{
-	printf("%s", s);
-}
-else
-{
-	printf("(nil)");
-}
-if (separator != NULL && i != n - 1)
-printf(%s, separator);
-}
-putchar ('\n');
+	char *s;
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(args, char *);
+		if (s != NULL)
+		{
+			printf("%s", s);
+		}
+		else
+		{
+			printf("(nil)");
+		}
+		if (separator != NULL && i != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	putchar ('\n');
 }
