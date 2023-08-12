1812b0dce3de16d1059a8ea8efe9806912c682db
Your Name <yassineholberton@gmail.com>
2023-08-12 23:07:28 +0100

    my task0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,21 +1,22 @@
 #include<stdarg.h>
 /**
 *sum_them_all - sum function
-*Return: 0
 *@n: variable
-*va_list - Declare a va_list
-*va_start - Initialize the va_list
-*va_end - clean the va_list
+*Return: 0
 */
 int sum_them_all(const unsigned int n, ...)
-unsigned int sum = 0 , int i;
-va_list args;
-if (!n)
-	return(0);
-va_starts (args  ,n);
-for (i = 0; i <n; i ++)
-	sum +=va_args (args, int);
-va_end(args);
-return(sum);
+{
+	int sum = 0;
+	unsigned int i;
+	va_list args;
 
-
+	if (!n)
+		return (0);
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+}
