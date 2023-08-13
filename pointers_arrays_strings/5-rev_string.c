0f86e81500a26535d9bd19ebe4338af45bd07b0a
Your Name <yassineholberton@gmail.com>
2023-08-13 11:55:08 +0100

    betty fix

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,19 +6,19 @@
 
 void rev_string(char *s)
 {
-    int length = 0;
-    char temp;
-    int i;
+	int length = 0;
+	char temp;
+	int i;
 
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+	while (*(s + length) != '\0')
+	{
+		length++;
+	}
 
-    for (i = 0; i < length / 2; i++)
-    {
-        temp = s[i];
-        s[i] = s[length - 1 - i];
-        s[length - 1 - i] = temp;
-    }
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
+	}
 }
