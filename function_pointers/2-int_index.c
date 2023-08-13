ac510713081ea896f1516e93f85cd674fd1a2719
Your Name <yassineholberton@gmail.com>
2023-08-13 15:41:23 +0100

    index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	if (cmp == NULL)
+	if (cmp == NULL || array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
