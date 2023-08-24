f0738f1c710ef20e3fca917bb76610e146f5dec3
Your Name <yassineholberton@gmail.com>
2023-08-24 11:30:20 +0100

    my tasks1230

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,21 +3,21 @@
  * _calloc - function to calloc
  * @nmemb: int
  * Return: to return the allocated memory
- * 
+ *
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    unsigned int i;
-    unsigned char *ptr;
-    void *allocated_memory;
+	unsigned int i;
+	unsigned char *ptr;
+	void *allocated_memory;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
-    allocated_memory = malloc(nmemb * size);
-    if (allocated_memory == NULL)
-        return NULL;
-    ptr = (unsigned char *)allocated_memory;
-    for (i = 0; i < nmemb * size; i++)
-    ptr[i] = 0;
-        return (allocated_memory);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	allocated_memory = malloc(nmemb * size);
+	if (allocated_memory == NULL)
+		return NULL;
+	ptr = (unsigned char *)allocated_memory;
+	for (i = 0; i < nmemb * size; i++)
+	ptr[i] = 0;
+		return (allocated_memory);
 }
