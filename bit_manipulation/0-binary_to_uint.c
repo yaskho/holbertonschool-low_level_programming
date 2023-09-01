6385d069b20d2d070eab86f6fd993546ab97127f
Your Name <yassineholberton@gmail.com>
2023-09-01 10:51:47 +0100

    my tasks

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to a string containing the binary number
+ *
+ * Return: the converted number, or 0 if there's an error
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i])
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		result = result * 2 + (b[i] - '0');
+		i++;
+	}
+
+	return (result);
+}
